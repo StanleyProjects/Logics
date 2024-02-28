85590d85753f263dcfe33b81aa693943ce5f8e71
Stanley Wintergreen <kepocnhh@gmail.com>
2024-02-28 17:55:41 +0300

    check readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Logics
 It is a business logic or UI level state holder.
+
+---
+
+## Unstable
+
+![version](https://img.shields.io/static/v1?label=version&message=0.0.1u-SNAPSHOT&labelColor=212121&color=2962ff&style=flat)
+
+- [Maven](https://s01.oss.sonatype.org/content/repositories/snapshots/com/github/kepocnhh/Logics/0.0.1u-SNAPSHOT)
+
+### Build
+```
+$ gradle lib:assembleUnstableJar
+```
+
+### Import
+```kotlin
+repositories {
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+    implementation("com.github.kepocnhh:Logics:0.0.1u-SNAPSHOT")
+}
+```
+
+---
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,5 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import sp.gx.core.Badge
+import sp.gx.core.Markdown
 import sp.gx.core.Maven
+import sp.gx.core.asFile
+import sp.gx.core.buildDir
+import sp.gx.core.check
+import sp.gx.core.create
 
 version = "0.0.1"
 
@@ -31,4 +37,32 @@ tasks.getByName<KotlinCompile>("compileTestKotlin") {
 
 dependencies {
 // todo
+}
+
+"unstable".also { variant ->
+    val version = "${version}u-SNAPSHOT"
+    tasks.create("check", variant, "Readme") {
+        doLast {
+            val badge = Markdown.image(
+                text = "version",
+                url = Badge.url(
+                    label = "version",
+                    message = version,
+                    color = "2962ff",
+                ),
+            )
+            val expected = setOf(
+                badge,
+                Markdown.link("Maven", Maven.Snapshot.url(maven, version)),
+                "implementation(\"${maven.moduleName(version)}\")",
+            )
+            rootDir.resolve("README.md").check(
+                expected = expected,
+                report = buildDir()
+                    .dir("reports/analysis/readme")
+                    .asFile("index.html"),
+            )
+        }
+    }
+    // todo
 }
