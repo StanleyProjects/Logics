e8793f74a45fdd7554ca77dc59acf848925c1c8e
Stanley Wintergreen <kepocnhh@gmail.com>
2024-02-28 21:46:24 +0300

    todo

--- lib/src/main/kotlin/sp/kx/logics/LogicsProvider.kt
+++ lib/src/main/kotlin/sp/kx/logics/LogicsProvider.kt
@@ -5,10 +5,18 @@ class LogicsProvider(
 ) {
     private data class CompositeKey<T : Logics>(val label: String, val type: Class<T>)
 
-    private val logics: MutableMap<CompositeKey<out Logics>, Logics> = mutableMapOf()
+    private val logics = mutableMapOf<CompositeKey<out Logics>, Logics>()
 
     fun <T : Logics> get(label: String, type: Class<T>): T {
         return logics.getOrPut(CompositeKey(label = label, type = type)) { factory.create(type) } as T
+    }
+
+    fun contains() {
+        TODO("LogicsProvider:contains")
+    }
+
+    fun remove() {
+        TODO("LogicsProvider:remove")
     }
 }
 
