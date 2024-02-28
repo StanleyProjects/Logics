aec1da5530828302658762e26d06023c42531eee
Stanley Wintergreen <kepocnhh@gmail.com>
2024-02-28 18:14:33 +0300

    unstable

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -3,9 +3,11 @@ import sp.gx.core.Badge
 import sp.gx.core.Markdown
 import sp.gx.core.Maven
 import sp.gx.core.asFile
+import sp.gx.core.assemble
 import sp.gx.core.buildDir
 import sp.gx.core.check
 import sp.gx.core.create
+import sp.gx.core.task
 
 version = "0.0.1"
 
@@ -64,5 +66,46 @@ dependencies {
             )
         }
     }
-    // todo
+    tasks.create("assemble", variant, "MavenMetadata") {
+        doLast {
+            val file = buildDir()
+                .dir("yml")
+                .file("maven-metadata.yml")
+                .assemble(
+                    """
+                        repository:
+                         groupId: '${maven.group}'
+                         artifactId: '${maven.id}'
+                        version: '$version'
+                    """.trimIndent(),
+                )
+            println("Metadata: ${file.absolutePath}")
+        }
+    }
+    task<Jar>("assemble", variant, "Jar") {
+        dependsOn(compileKotlinTask)
+        archiveBaseName = maven.id
+        archiveVersion = version
+        from(compileKotlinTask.destinationDirectory.asFileTree)
+    }
+    task<Jar>("assemble", variant, "Source") {
+        archiveBaseName = maven.id
+        archiveVersion = version
+        archiveClassifier = "sources"
+        from(sourceSets.main.get().allSource)
+    }
+    tasks.create("assemble", variant, "Pom") {
+        doLast {
+            val file = buildDir()
+                .dir("libs")
+                .file("${maven.name(version)}.pom")
+                .assemble(
+                    maven.pom(
+                        version = version,
+                        packaging = "jar",
+                    ),
+                )
+            println("POM: ${file.absolutePath}")
+        }
+    }
 }
