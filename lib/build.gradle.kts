34e4c2b0fd2b3c3cb516bf89561647b691b7ab6c
Stanley Wintergreen <kepocnhh@gmail.com>
2025-10-22 23:26:24 +0300

    0.2.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -19,7 +19,7 @@ import sp.gx.core.getByName
 import sp.gx.core.resolve
 import sp.gx.core.task
 
-version = "0.1.3"
+version = "0.2.0"
 
 val maven = Maven.Artifact(
     group = "com.github.kepocnhh",
